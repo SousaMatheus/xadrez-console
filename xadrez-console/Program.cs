441de906a8ab0308b111dd17cbdd565d6b70a895
Matheus Sousa <ma.sousa.soares@gmail.com>
2021-10-22 13:55:14 -0300

    melhorando metodo de imprimirTabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,9 +8,26 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(2, 0));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branco), new Posicao(5, 5));
+
+                Tela.ImprimirTabuleiro(tab);
+
+                Console.ReadLine();
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+
+            }
         }
     }
 }
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -10,6 +10,7 @@ namespace xadrez_console
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i +" ");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.Peca(i,j) == null)
@@ -18,10 +19,26 @@ namespace xadrez_console
                     }
                     else
                     {
-                        Console.Write(tab.Peca(i,j) + " ");
+                        imprimirPeca(tab.Peca(i,j));
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");            
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branco)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;// tipo consolecolor recebe a cor do caractere exibido, por padrão é cinza
+                Console.ForegroundColor = ConsoleColor.Yellow;//se for preto caractere recebe cor amarela
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
