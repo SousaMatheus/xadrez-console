4a5edc9f6bbed515d9dad60f4d3851bd7f55a37f
Matheus Sousa <ma.sousa.soares@gmail.com>
2021-11-03 22:09:46 -0300

    Acrescentando as demais pecas

---
+++ xadrez-console/xadrez/Cavalo.cs
@@ -0,0 +1,68 @@
+﻿using tabuleiro;
+namespace xadrez
+{
+    class Cavalo : Peca
+    {
+        public Cavalo(Tabuleiro tab,  Cor cor) : base (cor, tab)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "C";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+        public override bool[,] movimentosPossiveis()
+        {
+            bool [,] mat = new bool [Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            pos.definirValores(pos.Linha - 1, pos.Coluna - 2);
+            if (Tab.posicaoValida(pos) && podeMover(pos)){
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(pos.Linha - 2, pos.Coluna - 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(pos.Linha - 2, pos.Coluna + 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(pos.Linha - 1, pos.Coluna + 2);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(pos.Linha + 1, pos.Coluna + 2);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(pos.Linha + 2, pos.Coluna + 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(pos.Linha +2, pos.Coluna - 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(pos.Linha + 1, pos.Coluna - 2);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            return mat;
+        }
+    }
+}
---
+++ xadrez-console/xadrez/Dama.cs
@@ -0,0 +1,118 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Dama : Peca
+    {
+        public Dama(Tabuleiro tab, Cor cor) : base(cor, tab)
+        {
+        }
+        
+        public override string ToString()
+        {
+            return "D";
+        }
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = Tab.Peca(pos);
+            return p == null | p.Cor != Cor;
+        }
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //acima
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)// verifica se tem uma peça adversaria na posição
+                {
+                    break;//caso seja verdade encerra o while
+                }
+                pos.definirValores(pos.Linha -1, pos.Coluna); // verifica a proxima linha acima, toda vez que roda o while diminui 1
+            }
+            //abaixo
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)// verifica se tem uma peça adversaria na posição
+                {
+                    break;//caso seja verdade encerra o while
+                }
+                pos.definirValores(pos.Linha +1, pos.Coluna); // verifica a proxima linha abaixo
+            }
+            //direita
+            pos.definirValores(Posicao.Linha, Posicao.Coluna + 1);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)// verifica se tem uma peça adversaria na posição
+                {
+                    break;//caso seja verdade encerra o while
+                }
+                pos.definirValores(pos.Linha, pos.Coluna +1); // verifica a proxima coluna a direita
+            }
+            //esquerda
+            pos.definirValores(Posicao.Linha, Posicao.Coluna - 1);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)// verifica se tem uma peça adversaria na posição
+                {
+                    break;//caso seja verdade encerra o while
+                }
+                pos.definirValores(pos.Linha, pos.Coluna -1); // verifica a proxima a esquerda
+            }
+            //NO
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)// verifica se tem uma peça adversaria na posição
+                {
+                    break;//caso seja verdade encerra o while
+                }
+                pos.definirValores(pos.Linha - 1, pos.Coluna - 1);
+            }
+            //NE
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.Linha - 1, pos.Coluna + 1);
+
+            }
+            //SE
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.Linha + 1, pos.Coluna + 1);
+            }
+            //SO
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.Linha + 1, pos.Coluna - 1);
+            }
+            return mat; //retorna matriz como resposta
+        }
+    }
+}
